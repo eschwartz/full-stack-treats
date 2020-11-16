a24f7752ca81bced44c544cedd1fccd340830c26
Edan Schwartz <edanschwartz@gmail.com>
2020-11-16 15:52:11 -0600

    Revert "unit test results"
    This reverts commit 9a678840e82c1105224444f8117de0ae59276f80.

--- test.sh
+++ test.sh
@@ -23,7 +23,7 @@ echo "Running tests...."
 reportersFlag=''
 if [[ -n "${CI}" ]]; then
   set -e
-  reportersFlag="--reporters=jest-github-actions-reporter --reporters=jest-junit"
+  reportersFlag="--reporters=jest-github-actions-reporter"
 fi
 
 TEST_DB=${testDb} \
