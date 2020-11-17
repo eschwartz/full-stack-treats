f158f92dd2ef7cb31314ef8456760e3c7a972c1a
Edan Schwartz <edanschwartz@gmail.com>
2020-11-17 14:29:39 -0600

    toBe2xx

--- test/server.test.js
+++ test/server.test.js
@@ -1,6 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-const pg = require('pg');
 const pool = require('../server/modules/pool');
 const supertest = require('supertest');
 const app = require('../server/server');
@@ -8,6 +5,14 @@ const app = require('../server/server');
 let agent;
 let querySpy;
 
+expect.extend({
+  toBe2xx(statusCode) {
+    return {
+      pass: statusCode >= 200 && statusCode < 300,
+      message: () => `expected a 2xx status code`
+    }
+  },
+});
 
 beforeAll(async() => {
   // Update the app's pool, to connect to our test database
@@ -17,6 +22,7 @@ beforeAll(async() => {
 
   // Spy on pool.query
   querySpy = jest.spyOn(pool, 'query');
+  querySpy.lastCall = () => querySpy.mock.calls[querySpy.mock.calls.length-1]
 
   // Set the supertest agent
   agent = supertest.agent(app)
@@ -30,7 +36,7 @@ describe('GET /treats', () => {
 
   it('should return treats from the database', async() => {
     let res = await agent.get('/treats');
-    expect(res.statusCode, 'GET /treats should return a 200').toBe(200);
+    expect(res.statusCode, 'GET /treats should return a 200').toBe2xx(200);
     expect(res.body, 'GET /treats should return data from the DB').toMatchObject([
       { 
         id: 1,
@@ -80,14 +86,10 @@ describe('PUT /treats', () => {
   it('should update a treat in the database', async() => {
     let putRes = await agent.put('/treats/1')
       .send({
-          // NOTE: README says PUT only needs to update description, 
-          // even though app supports name+pic
           description: 'Like a cupcake, but bowl-sized!', 
-          // name: 'Bowl Cake', 
-          // pic: '/assets/cupcake.jpg'
       });
 
-    expect(putRes.statusCode, 'PUT /treats should return a 200').toBe(200);
+    expect(putRes.statusCode, 'PUT /treats should return a 200').toBe2xx();
 
     // Check the DB, that our record was updated
     let dbRes = await pool.query(`
@@ -96,9 +98,27 @@ describe('PUT /treats', () => {
     `);
     expect(dbRes.rows[0], 'should update the data in the DB').toMatchObject({
       description: 'Like a cupcake, but bowl-sized!', 
-      // name: 'Bowl Cake', 
-      // pic: '/assets/cupcake.jpg' 
     });
+
+    // Check that other records are NOT updated
+    dbRes = await pool.query(`
+      SELECT * FROM "treats"
+      WHERE id=2
+    `);
+    expect(dbRes.rows[0], 'should not alter other rows in the database').toMatchObject({
+      id: 2,
+      description: 'Mmmm donuts', 
+    });
+  });
+
+  it('should use parameterized queries', async() => {
+    await agent.put('/treats/1')
+      .send({
+          description: 'Like a cupcake, but bowl-sized!', 
+      });
+
+    expect(querySpy.lastCall()[1], 'passes description as SQL parameter')
+      .toContain('Like a cupcake, but bowl-sized!')
   });
 
 });
@@ -114,7 +134,7 @@ describe('POST /treats', () => {
         pic: '/assets/potato.jpg' 
       });
 
-    expect(postRes.statusCode, 'POST /treats should return a 201').toBe(201);
+    expect(postRes.statusCode, 'POST /treats should return a 201').toBe2xx();
 
     // Check that the item was saved to the DB
     let dbRes = await pool.query(`
@@ -129,13 +149,29 @@ describe('POST /treats', () => {
     });
   });
 
+  it('should use parameterized queries', async() => {
+    await agent.post('/treats')
+      .send({ 
+        name: 'Potato', 
+        description: 'Everyone loves a good potato',
+        pic: '/assets/potato.jpg' 
+      });
+
+    expect(querySpy.lastCall()[1], 'passes name as SQL parameter')
+      .toContain('Potato');
+    expect(querySpy.lastCall()[1], 'passes description as SQL parameter')
+      .toContain('Everyone loves a good potato');
+    expect(querySpy.lastCall()[1], 'passes pic as SQL parameter')
+      .toContain('/assets/potato.jpg');
+  });
+
 });
 
 describe('DELETE /treats/:id', () => {
 
   it('should delete a treat from the database', async () => {
     let res = await agent.delete('/treats/1');
-    expect(res.statusCode, 'should return a 200 response code').toBe(200);
+    expect(res.statusCode, 'should return a 200 response code').toBe2xx();
 
 
     // Look for treat 1 in the DB
@@ -145,15 +181,20 @@ describe('DELETE /treats/:id', () => {
     `);
     expect(dbRes.rows.length, 'record should not be in the database').toBe(0);
 
+    // Should not delete other rows
+    dbRes = await pool.query(`
+      SELECT * FROM treats
+      WHERE id=2
+    `);
+    expect(dbRes.rows.length, 'should not delete other records').toBe(1);
   });
 
   it('should use SQL prepared statements', async() => {
     await agent.delete('/treats/1');
 
     // Inspect the last call to `pool.query`
-    let queryCall = querySpy.mock.calls[querySpy.mock.calls.length-1];
-
-    expect(queryCall[1], 'should pass in the treat ID as a SQL parameter').toEqual([1]);
+    // and check that we passed SQL params to it
+    expect(querySpy.lastCall()[1], 'should pass in the treat ID as a SQL parameter').toEqual(['1']);
   });
 
 });
